c49cffb656bad9ef93637991f39aedfa93f2d54f
Filip Nikolov <filip.d.nikolov95@gmail.com>
2023-10-24 21:27:01 +0200

    DRF Generic Create APIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# .../api/products/
 urlpatterns = [
+    path('', views.ProductCreateAPIView.as_view()), # leave the path empty because in our cfehome/urls.py we already have the path declared as /api/products and this would create it as /api/products//
     path('<int:pk>/', views.ProductDetailAPIView.as_view())
 ]
