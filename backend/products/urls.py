da001a3d4c4680fdb63dac7580c3cbcd5c1a8b5d
Filip Nikolov <filip.d.nikolov95@gmail.com>
2023-11-04 21:17:04 +0100

    URLs, Reverse and Serializers

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 # .../api/products/
 urlpatterns = [
-    path('', views.ProductListCreateAPIView.as_view()), # leave the path empty because in our cfehome/urls.py we already have the path declared as /api/products and this would create it as /api/products//
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+    path('', views.ProductListCreateAPIView.as_view(), name = 'product-list'), # leave the path empty because in our cfehome/urls.py we already have the path declared as /api/products and this would create it as /api/products//
+    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name = 'product-detail'),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name = 'product-edit'),
     path('<int:pk>/destroy/', views.ProductDestroyAPIView.as_view())
 ]
